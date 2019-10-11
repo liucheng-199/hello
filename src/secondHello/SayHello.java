a56728250502933e17793f40097b84a8e5b95018
liucheng-199 <1184418778@qq.com>
2019-10-11 10:51:28 +0800

    second commit

--- src/secondHello/SayHello.java
+++ src/secondHello/SayHello.java
@@ -2,6 +2,6 @@ package secondHello;
 
 public class SayHello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world!!!");
     }
 }
