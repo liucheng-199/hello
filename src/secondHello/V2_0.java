b58acd9c2479185a60d6366b15e30c7b4cce14e7
liucheng-199 <1184418778@qq.com>
2019-10-11 14:11:50 +0800

    2.0 add a()

--- src/secondHello/V2_0.java
+++ src/secondHello/V2_0.java
@@ -3,5 +3,10 @@ package secondHello;
 public class V2_0 {
     public static void main(String[] args) {
         System.out.println("2.0");
+
     }
+    public void a(){
+            System.out.println("!!!!!");
+
+}
 }
